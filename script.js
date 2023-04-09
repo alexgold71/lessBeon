0e6407a6feb2bcc4c0c58e002c4b3a14ae3d6fd8
Aleksandr <webxxx@inbox.ru>
2023-04-09 16:16:31 +0300

    1st commit

---
+++ script.js
@@ -0,0 +1,35 @@
+'ude strict';
+
+
+
+// console.log(4/0);
+// console.log('string'*9);
+ 
+// let person = { 
+//     name: "John", 
+//     age:25,
+//     isMarried: false
+// }
+
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[2]);
+
+// let answear = +prompt("Are you 18 year old? Yes or No", "Yes");
+// console.log(typeof(answear));
+
+// console.log("arr" + "- object");
+// console.log(4 + "- object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+let isChecked = false,
+    isClosed = false;
+
+console.log(isChecked || !isClosed);
