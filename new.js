689777eaf89d183c3f4a2dc0d3e5c0d9c680d506
Aleksandr <webxxx@inbox.ru>
2023-04-11 13:05:22 +0300

    Добавлены news.js и workout.html по домашнему заданию

--- new.js
+++ new.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let money,
+    time;
+
+
+money = prompt("Ваш бюджет в месяц?", '');
+time = prompt("Введите дату в формате YYYY-MM-DD", '');    
+
+
+let mustHave = prompt("Введите обязательную статью расходов в этом месяце", '');
+let howMuch = prompt("Во сколько обойдется?");
+
+let expenses = {
+    mustHave: howMuch,
+}
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: expenses,
+    optionalExpenses: '',
+    income: '',
+    savings: false,
+
+}
+
+console.log("Бюджет на 1 месяц =", appData.budget / 30);
+console.log(mustHave);
