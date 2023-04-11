a375f535a91ceb6bf964c950144c33b24c4b79f8
Aleksandr <webxxx@inbox.ru>
2023-04-11 13:14:54 +0300

    Правки news.js

--- new.js
+++ new.js
@@ -1,30 +1,24 @@
 'use strict';
 
-let money,
-    time;
-
-
-money = prompt("Ваш бюджет в месяц?", '');
-time = prompt("Введите дату в формате YYYY-MM-DD", '');    
-
-
-let mustHave = prompt("Введите обязательную статью расходов в этом месяце", '');
-let howMuch = prompt("Во сколько обойдется?");
-
-let expenses = {
-    mustHave: howMuch,
-}
-
+let money = prompt("Ваш бюджет в месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: expenses,
+    expenses: {},
     optionalExpenses: '',
-    income: '',
-    savings: false,
-
+    income: [],
+    savings: false
 }
+    
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?");
 
-console.log("Бюджет на 1 месяц =", appData.budget / 30);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert("Бюджет на 1 месяц =", appData.budget / 30);
 console.log(mustHave);
